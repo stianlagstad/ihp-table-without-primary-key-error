44b7e8c7a705029fd717b4bf31ff9f3b89360764
Stian Lågstad <stianlagstad@gmail.com>
2021-01-18 21:14:09 +0100

    Trigger error

---
+++ Application/Fixtures.sql
@@ -0,0 +1,30 @@
+
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+
+SET SESSION AUTHORIZATION DEFAULT;
+
+ALTER TABLE public.first DISABLE TRIGGER ALL;
+
+
+
+ALTER TABLE public.first ENABLE TRIGGER ALL;
+
+
+ALTER TABLE public.second DISABLE TRIGGER ALL;
+
+
+
+ALTER TABLE public.second ENABLE TRIGGER ALL;
+
+
--- Application/Schema.sql
+++ Application/Schema.sql
@@ -1,1 +1,8 @@
 -- Your database schema. Use the Schema Designer at http://localhost:8001/ to add some tables.
+CREATE TABLE "first" (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL
+);
+CREATE TABLE "second" (
+    first_id UUID NOT NULL
+);
+ALTER TABLE "second" ADD CONSTRAINT second_ref_first_id FOREIGN KEY (first_id) REFERENCES "first" (id) ON DELETE NO ACTION;
